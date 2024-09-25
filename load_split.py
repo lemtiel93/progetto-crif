19dd8440888b083cc45da517944fc767f2a19d23
GianfriShow <131198417+GianfriShow@users.noreply.github.com>
2024-09-25 16:38:19 +0200

    Add files via upload

--- load_split.py
+++ load_split.py
@@ -1,9 +1,9 @@
 import numpy as np
-import pandas as pd
+import pandas 
 
 def load_and_split(path):
 
-    df = pd.read_csv(path)
+    df = pandas.read_csv(path)
     dataset = df['pixels'].apply(lambda x: np.fromstring(x, sep=' ', dtype=int)).values
     dataset = np.stack(dataset).reshape(-1, 48, 48, 1)
     dataset_labels = df.drop(['pixels', 'usage'], axis=1)
@@ -20,4 +20,4 @@ def load_and_split(path):
     x_test = dataset[test_indices]
     y_test = dataset_labels[test_indices]
 
-    return x_train,y_train,x_val,y_val,x_test,y_test
+    return (x_train,y_train),(x_val,y_val),(x_test,y_test)
